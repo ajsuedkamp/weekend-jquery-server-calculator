2ef8026b1dd4a11b9a7cc5c503501028df61997c
Alex Suedkamp <ajsuedkamp@gmail.com>
2022-07-31 11:39:19 -0500

    completed the frame work of a post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,17 +4,29 @@ $(readyNow);
 
 function readyNow() {
     console.log('readyNow');
+    $('#equals').on('click', sendAnswerToServer)
     $('#add').on('click', add);
 }
 
 
-
-function add() {
-    let firstNumber = parseInt($('#number-one').val());
-    let secondNumber = parseInt($('#number-two').val());
-    let sum = (firstNumber + secondNumber);
-    $('#answer').append(`
-        ${sum}
-    `)
+function sendAnswerToServer() {
+    $.ajax({
+        type: 'POST',
+        url: '/answers',
+        data: {
+            firstNumber: $('#number-one').val(),
+            secondNumber: $('number-two').val(),
+        }
+    })
 }
+
+// working addition function
+// function add() {
+//     let firstNumber = parseInt($('#number-one').val());
+//     let secondNumber = parseInt($('#number-two').val());
+//     let sum = (firstNumber + secondNumber);
+//     $('#answer').append(`
+//         ${sum}
+//     `)
+// }
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
 const app = express();
-
 const port = process.env.PORT || 5000;
-
 app.use(express.static('server/public'));
+app.use(express.urlencoded());
+
+const equationEntered = [{
+    firstNumber: ,
+    secondNumber: ,
+    operation:
+}]
+
+app.post('/answers', (req, res) => {
+    const equation = req.body;
+
+});
+
+
+
+
 
 app.listen(port, () => {
     console.log('Listening on port', port);
